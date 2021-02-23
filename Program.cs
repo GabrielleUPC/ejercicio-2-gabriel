80aeee0d6a79b4d36ff86958075efc1413a14eb3
GabrielleUPC <79283702+GabrielleUPC@users.noreply.github.com>
2021-02-22 23:48:41 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CSharp_Shell
+{
+
+    class Punto 3
+    {
+    static void Main(string[] args)
+    {
+        int edad;
+
+        Console.Write("Favor ingresar la edad: ");
+        edad = int.Parse(Console.ReadLine());
+
+        Console.Write("La cantidad de pulsaciones por cada diez segundos deberia ser de: " +)
+        ((220 - edad) / 10));
+
+        Console.ReadKey();
+    }
+
+}
+}
